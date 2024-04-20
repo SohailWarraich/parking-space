55725cfcda9c30bab5ed9c9c27433e796ca61d27
sohail <arshadsohail092@gmail.com>
2024-04-21 02:20:35 +0500

    compare images with labels

---
+++ remove_extra_labels.py
@@ -0,0 +1,18 @@
+import os
+
+# Set the paths to the folders
+image_folder = r'C:\Users\jarsh\Downloads\test\images'
+label_folder = r'C:\Users\jarsh\Downloads\test\labels'
+
+# Get the list of image files
+image_files = os.listdir(image_folder)
+
+# Get the list of label files
+label_files = os.listdir(label_folder)
+
+# Iterate over the label files
+for label_file in label_files:
+    # Check if the corresponding image file exists
+    if label_file.replace('.txt', '.jpg') not in image_files:
+        # Delete the extra label file
+        os.remove(os.path.join(label_folder, label_file))
